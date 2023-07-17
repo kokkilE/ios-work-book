ecf7f941ed77e3c78891d7f13cc246a099cf4bcf
kkuldak1@naver.com <kkuldak1@naver.com>
2023-07-17 21:10:22 +0900

    feat: Problem, Workbook 모델 구현

---
+++ Workbook/Workbook/Sources/Problem/Problem.swift
@@ -0,0 +1,12 @@
+//
+//  Problem.swift
+//  Workbook
+//
+//  Created by 조향래 on 2023/07/17.
+//
+
+struct Problem: Hashable {
+    let question: String
+    let example: String?
+    let answer: String
+}
---
+++ Workbook/Workbook/Sources/Workbook/Workbook.swift
@@ -0,0 +1,11 @@
+//
+//  Workbook.swift
+//  Workbook
+//
+//  Created by 조향래 on 2023/07/17.
+//
+
+struct Workbook: Hashable {
+    var title: String
+    var problems: [Problem]
+}
