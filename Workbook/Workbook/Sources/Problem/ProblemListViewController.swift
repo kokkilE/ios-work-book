5e963e4470310bb3b96e0d191badd0c5a4d3c682
kkuldak1@naver.com <kkuldak1@naver.com>
2023-07-17 22:48:11 +0900

    feat: ProblemControlView 구현, ProblemControlVC 기본 UI 구현

--- Workbook/Workbook/Sources/Problem/ProblemListViewController.swift
+++ Workbook/Workbook/Sources/Problem/ProblemListViewController.swift
@@ -8,7 +8,43 @@
 import UIKit
 
 final class ProblemListViewController: UIViewController {
+    private let viewModel: WorkbookViewModel
+    private let problemControlView = ProblemControlView()
+    
+    init(viewModel: WorkbookViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        addSubviews()
+        layout()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func addSubviews() {
+        view.addSubview(problemControlView)
+    }
+    
+    private func layout() {
+        let safe = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            problemControlView.topAnchor.constraint(equalTo: safe.topAnchor),
+            problemControlView.leadingAnchor.constraint(equalTo: safe.leadingAnchor),
+            problemControlView.trailingAnchor.constraint(equalTo: safe.trailingAnchor),
+            problemControlView.heightAnchor.constraint(equalTo: safe.heightAnchor, multiplier: 0.10)
+        ])
     }
 }
