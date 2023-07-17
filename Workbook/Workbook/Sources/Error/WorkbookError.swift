9f2024023feb0f7a0780f4c278dde461a431541b
kkuldak1@naver.com <kkuldak1@naver.com>
2023-07-17 21:21:17 +0900

    feat: WorkbookError 구현

---
+++ Workbook/Workbook/Sources/Error/WorkbookError.swift
@@ -0,0 +1,19 @@
+//
+//  WorkbookError.swift
+//  Workbook
+//
+//  Created by 조향래 on 2023/07/17.
+//
+
+import Foundation
+
+enum WorkbookError: LocalizedError {
+    case wrongSubscript
+    
+    var description: String {
+        switch self{
+        case .wrongSubscript:
+            return "wrong subscript"
+        }
+    }
+}
