4201f45d3d0800f5036189dd9486eca84ba6ba5f
kkuldak1@naver.com <kkuldak1@naver.com>
2023-07-26 16:06:00 +0900

    feat: 문항의 보기 입력을 받기 위한 ProblemExampleView 구현

--- Workbook/Workbook/Sources/Problem/ProblemAddView.swift
+++ Workbook/Workbook/Sources/Problem/ProblemAddView.swift
@@ -16,36 +16,37 @@ final class ProblemAddView: UIStackView {
     }()
     
     private let problemTitleView = ProblemTitleView()
+    private let problemExampleView = ProblemExampleView()
     
-    private let exampleTextFieldList = [UITextField]()
-    
-    private lazy var exampleStackView = {
-        let exampleLabel = UILabel()
-        exampleLabel.font = .systemFont(ofSize: 20)
-        exampleLabel.text = "문항 보기"
-        
-        let titleTextField = UITextField()
-        titleTextField.placeholder = "문항의 보기를 입력하세요."
-        titleTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        
-        let bottomBorder = UIView()
-        bottomBorder.backgroundColor = .systemGray4
-        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
-        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true
-        
-        let textFieldStackView = UIStackView(arrangedSubviews: [titleTextField, bottomBorder])
-        textFieldStackView.spacing = 4
-        textFieldStackView.layoutMargins = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
-        textFieldStackView.isLayoutMarginsRelativeArrangement = true
-        textFieldStackView.axis = .vertical
-        
-        let stackView = UIStackView(arrangedSubviews: [exampleLabel, textFieldStackView])
-        stackView.spacing = 4
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-                
-        return stackView
-    }()
+//    private let exampleTextFieldList = [UITextField]()
+//
+//    private lazy var exampleStackView = {
+//        let exampleLabel = UILabel()
+//        exampleLabel.font = .systemFont(ofSize: 20)
+//        exampleLabel.text = "문항 보기"
+//
+//        let titleTextField = UITextField()
+//        titleTextField.placeholder = "문항의 보기를 입력하세요."
+//        titleTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//
+//        let bottomBorder = UIView()
+//        bottomBorder.backgroundColor = .systemGray4
+//        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+//        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true
+//
+//        let textFieldStackView = UIStackView(arrangedSubviews: [titleTextField, bottomBorder])
+//        textFieldStackView.spacing = 4
+//        textFieldStackView.layoutMargins = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+//        textFieldStackView.isLayoutMarginsRelativeArrangement = true
+//        textFieldStackView.axis = .vertical
+//
+//        let stackView = UIStackView(arrangedSubviews: [exampleLabel, textFieldStackView])
+//        stackView.spacing = 4
+//        stackView.axis = .vertical
+//        stackView.alignment = .fill
+//
+//        return stackView
+//    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -67,6 +68,6 @@ final class ProblemAddView: UIStackView {
     private func addSubviews() {
         addArrangedSubview(segmentedControl)
         addArrangedSubview(problemTitleView)
-        addArrangedSubview(exampleStackView)
+        addArrangedSubview(problemExampleView)
     }
 }
---
+++ Workbook/Workbook/Sources/Problem/ProblemExampleView.swift
@@ -0,0 +1,67 @@
+//
+//  ProblemExampleView.swift
+//  Workbook
+//
+//  Created by 조향래 on 2023/07/26.
+//
+
+import UIKit
+
+final class ProblemExampleView: UIStackView {
+    private var exampleTextFieldList = [UITextField]()
+    
+    private let textFieldStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 12
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.layoutMargins = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        addTextField()
+        addTextField()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        let exampleLabel = UILabel()
+        exampleLabel.font = .systemFont(ofSize: 20)
+        exampleLabel.text = "문항 보기"
+        exampleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        let addImage = UIImage(systemName: "plus")
+        let addExampleButton = UIButton()
+        addExampleButton.setImage(addImage, for: .normal)
+        addExampleButton.addTarget(self, action: #selector(addTextField), for: .touchUpInside)
+        addExampleButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        
+        let exampleStackView = UIStackView(arrangedSubviews: [exampleLabel, addExampleButton])
+        exampleStackView.axis = .horizontal
+        
+        addArrangedSubview(exampleStackView)
+        addArrangedSubview(textFieldStackView)
+        spacing = 4
+        axis = .vertical
+        alignment = .fill
+    }
+    
+    @objc private func addTextField() {
+        let titleTextField = UITextField()
+        titleTextField.placeholder = "문항의 보기를 입력하세요."
+        titleTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        exampleTextFieldList.append(titleTextField)
+        
+        textFieldStackView.addArrangedSubview(titleTextField)
+    }
+}
