2acb56ba56b07a2bfd9c6887b304064338644374
kkuldak1@naver.com <kkuldak1@naver.com>
2023-08-04 10:59:36 +0900

    feat: Problem 모델에 객관식, 주관식을 지정하는 enum 타입 추가 구현

--- Workbook/Workbook/Sources/Problem/Model/Problem.swift
+++ Workbook/Workbook/Sources/Problem/Model/Problem.swift
@@ -6,6 +6,29 @@
 //
 
 struct Problem: Hashable {
+    enum ProblemType: Hashable {
+        case shortAnswer
+        case multipleChoice
+        
+        var description: String {
+            switch(self) {
+            case .shortAnswer:
+                return "주관식"
+            case .multipleChoice:
+                return "객관식"
+            }
+        }
+        
+        var index: Int {
+            switch(self) {
+            case .shortAnswer:
+                return 0
+            case .multipleChoice:
+                return 1
+            }
+        }
+    }
+    
     let question: String
     let example: String?
     let answer: String
