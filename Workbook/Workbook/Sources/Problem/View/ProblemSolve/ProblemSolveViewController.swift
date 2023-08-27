bee247b180431f7ff39fdd534b6223b71137f1a0
kkuldak1@naver.com <kkuldak1@naver.com>
2023-08-27 18:10:43 +0900

    feat: 문제 풀이 진행 상황을 표시할 레이블 정의

--- Workbook/Workbook/Sources/Problem/View/ProblemSolve/ProblemSolveViewController.swift
+++ Workbook/Workbook/Sources/Problem/View/ProblemSolve/ProblemSolveViewController.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 class ProblemSolveViewController: UIViewController {
-
+    private let viewModel = ProblemViewModel()
+    private let progressLabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        setupNavigationItems()
     }
     
     private func setupView() {
         view.backgroundColor = .white
+    }
+    
+    private func setupNavigationItems() {
+        setupNavigationTitle()
+    }
+    
+    private func setupNavigationTitle() {
+        navigationItem.titleView = progressLabel
     }
 }
