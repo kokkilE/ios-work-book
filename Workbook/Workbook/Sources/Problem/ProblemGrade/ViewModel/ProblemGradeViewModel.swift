bbe5f18d0f4bad3eba1a1d82ce3c81e4da05532a
kkuldak1@naver.com <kkuldak1@naver.com>
2023-09-05 14:12:49 +0900

    feat: VM에 사용자 입력과 문제의 정답을 비교하는 채점 기능 구현

--- Workbook/Workbook/Sources/Problem/ProblemGrade/ViewModel/ProblemGradeViewModel.swift
+++ Workbook/Workbook/Sources/Problem/ProblemGrade/ViewModel/ProblemGradeViewModel.swift
@@ -11,9 +11,44 @@ final class ProblemGradeViewModel: UserAnswerProcessing {
     
     var selectedWorkbook: Workbook?
     var userAnswerList: [ProblemUserAnswer]
+    private var wrongProblemIndex = Set<Int>()
+    var selectedWorkbookCount: Int? {
+        get {
+            selectedWorkbook?.getProblemsCount()
+        }
+    }
+    var correctProblemCount: Int? {
+        get {
+            guard let selectedWorkbookCount else { return nil }
+            
+            return selectedWorkbookCount - wrongProblemIndex.count
+        }
+    }
     
     init(_ viewModel: UserAnswerProcessing) {
         selectedWorkbook = viewModel.selectedWorkbook
         userAnswerList = viewModel.userAnswerList
+        grade()
+    }
+    
+    private func grade() {
+        guard let selectedWorkbook else { return }
+        
+        let indexRange = userAnswerList.startIndex...userAnswerList.endIndex
+        
+        for index in indexRange {
+            guard let userAnswer = userAnswerList[safe: index] else { continue }
+            
+            switch userAnswer.problemType {
+            case .shortAnswer:
+                if userAnswer.shortAnswer?.getExceptSpaces() == selectedWorkbook.getProblem(at: index)?.shortAnswer?.getExceptSpaces() {
+                    wrongProblemIndex.insert(index)
+                }
+            case .multipleChoice:
+                if userAnswer.multipleAnswer == selectedWorkbook.getProblem(at: index)?.multipleAnswer {
+                    wrongProblemIndex.insert(index)
+                }
+            }
+        }
     }
 }
